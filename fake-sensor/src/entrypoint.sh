444f586850c8ca7437bea9ea01000734b55944dd
Konstantin Strümpf <k.struempf@icloud.com>
2020-11-26 22:04:12 +0100

    [#5] Using exec in entrypoint.sh (replaces current process -> script receives signals)
    See: https://itnext.io/containers-terminating-with-grace-d19e0ce34290

--- fake-sensor/src/entrypoint.sh
+++ fake-sensor/src/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python3 -u fakesensor.py --host "$HOST" --port "$PORT" --topic "$TOPIC" --columns $COLUMNS --time time --replay-speed "$REPLAY_SPEED" --keep-connection "$KEEP_CONNECTION" --client-id "$CLIENT_ID" $FILES
+exec python3 -u fakesensor.py --host "$HOST" --port "$PORT" --topic "$TOPIC" --columns $COLUMNS --time time --replay-speed "$REPLAY_SPEED" --keep-connection "$KEEP_CONNECTION" --client-id "$CLIENT_ID" $FILES
