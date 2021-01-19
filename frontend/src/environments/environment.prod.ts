08b721cf51e7fcd6fdf11fa4a74a2895a32d3fb7
Konstantin Strümpf <k.struempf@icloud.com>
2021-01-19 12:02:13 +0100

    [#7] Added frontend to GitLab CI, adapted environment.prod.ts to fix Docker build issues for now

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
+// TODO: this should change an load secrets from the backend
 export const environment = {
-  production: true
+  production: true,
+  blockChainAddressWS: null,
+  blockChainAddressHttp: null,
+  dataFilterAddress: null,
+  contractAddress: null,
+  privateAccountKey: null
 };
