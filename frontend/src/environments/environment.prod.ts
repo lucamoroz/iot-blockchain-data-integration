aefc91d0b078f329145aa028dc4af4bef6e6f7fe
Konstantin Strümpf <k.struempf@icloud.com>
2021-01-19 18:39:09 +0100

    [#7] Added static IP to angular prod env, added frontend to prod docker-compose

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 // TODO: this should change an load secrets from the backend
 export const environment = {
   production: true,
-  dataFilterAddress: null,
+  dataFilterAddress: 'http://35.207.109.228:5000'
+  // dataFilterAddress: 'http://127.0.0.1:5000'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  dataFilterAddress: 'http://127.0.0.1',
+  dataFilterAddress: 'http://127.0.0.1'
 };
 
 /*
